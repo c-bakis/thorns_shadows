9b03ca5d4ad6a692d2acc003d33387009e5f61a7
Christina Bakis <c.bakis@gmx.de>
2026-07-16 12:54:45 +0200

    docs: add JSDoc

--- js/dialog.js
+++ js/dialog.js
@@ -5,21 +5,31 @@
     const settingsBtn = document.getElementById('settings-btn');
     const infoOverlay = document.getElementById('info-overlay-dialog');
 
-
+/**
+ * Opens a dialog element if it exists and supports the showModal method.
+ * @param {HTMLDialogElement} dialog
+ */
 function openDialog(dialog) {
     if (dialog && typeof dialog.showModal === 'function') {
         dialog.showModal();
     }
 }
 
-// Check if the click is outside the dialog content
+/**
+ * Closes a dialog element if it exists and supports the close method.
+ * @param {Event} event
+ * @param {HTMLDialogElement} dialog
+ */
 function closeDialogOnClickOutside(event, dialog) {
   if (event.target === dialog) {
     closeDialog(dialog);
   }
 }
 
-// Check if the dialog is open before trying to close it
+/**
+ * Closes a dialog element if it exists and supports the close method.
+ * @param {HTMLDialogElement} dialog
+ */
 function closeDialog(dialog) {
   if (dialog && dialog.open) {
     dialog.close();
